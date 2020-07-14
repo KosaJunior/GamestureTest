b4e5806c8cb6fc50f0596128b981ae1f0411268d
KosaJr <kosajunior@gmail.com>
2020-07-14 09:06:36 +0200

    feature(Scripts/ShowTextButtonController): added ShowTextButtonController

---
+++ GamestureTest/Assets/Scripts/ShowTextButtonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GamestureTest
+{
+	public class ShowTextButtonController : MonoBehaviour
+	{
+		#region variables
+
+		[Header("[ Components references ]")]
+		[SerializeField] private Button showTextButton;
+		[SerializeField] private GameObject textToShow;
+
+		[Header("[ Settings  ]")]
+		[SerializeField] private float displayTime = 3.0f;
+
+		#endregion variables
+
+		#region public methods
+
+		public void ShowText() // inspector method
+		{
+			StartCoroutine(ShowTextCoroutine());
+			SetTextVisibility(true);
+		}
+
+		#endregion public methods
+
+		#region private methods
+
+		private IEnumerator ShowTextCoroutine()
+		{
+			yield return new WaitForSeconds(displayTime);
+
+			SetTextVisibility(false);
+		}
+
+		private void SetTextVisibility(bool isVisible)
+		{
+			showTextButton.interactable = !isVisible;
+			textToShow.SetActive(isVisible);
+		}
+
+		#endregion private methods
+	}
+}
